094d6a9d5a61c83c330cc5d9803b637d8096800d
uwadonat <uwadonat@gmail.com>
2021-04-27 03:25:55 -0400

    remove sql lite

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -1,17 +1,14 @@
 class FollowingsController < ApplicationController
-  def index
-    @followers = Following.all
-    @user = User.all.order('created_at DESC')
-  end
-
-  def create
-    @user = User.find(params[:format])
-    @follower = current_user.followings.create(followed_id: params[:format])
-    @follower.save
-    redirect_back(fallback_location: root_path)
-  end
-
-  def followers
-    @foll = Following.where(followed_id: current_user.id).count
-  end
+   def index
+     @following = Following.new
+     @followers = Following.all
+   end
+   def create
+      @user = User.find(params[:format])
+      
+      @follower =current_user.followings.create(followed_id: params[:format])
+      @follower.save
+        redirect_back(fallback_location: root_path)
+   end
+ 
 end
