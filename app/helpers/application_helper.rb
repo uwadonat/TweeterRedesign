4c8f70d692e19d8416218749a0251d0f1da1c061
uwadonat <uwadonat@gmail.com>
2021-04-28 11:33:15 -0400

    Add animation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,49 @@ module ApplicationHelper
       link_to('Like!', technology_likes_path(technology_id: technology.id), method: :post)
     end
   end
+
+  def followingss(user)
+    display = current_user.id != user.id && !current_user.followings.where(followed_id: user.id).any?
+    if display
+        link_to('follow', followings_path(@user), method: :post)
+    else
+        link_to ('followed')
+    end
+
+  end
+
+
+  def followerss_image(user)
+    dis = current_user.id != user.id && !current_user.followings.where(followed_id: user.id).any?
+    if dis
+        (image_tag user.profile_photo_url, width: '32px', class:'followee-image')
+    end
+
+  end
+
+  def followerss_name(user)
+    dis = current_user.id != user.id && !current_user.followings.where(followed_id: user.id).any?
+    if dis
+        (user.fullname)
+    end
+
+  end
+
+  def followerss_email(user)
+    dis = current_user.id != user.id && !current_user.followings.where(followed_id: user.id).any?
+    if dis
+        (user.email)
+    end
+
+  end
+
+  def followerss_link(user)
+    dis = current_user.id != user.id && !current_user.followings.where(followed_id: user.id).any?
+    if dis
+        link_to('follow', followings_path(user), method: :post)
+    end
+
+  end
+
+
 end
